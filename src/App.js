99acef9fbdfdb372b94e1a8c5c9715964e7a58b7
LXiangTYUT <lxiang_ty@icloud.com>
2017-04-26 19:00:05 +0800

    修改readme.md

--- src/App.js
+++ src/App.js
@@ -1,36 +1,63 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SQLSUtils from  './dao/sqlserverUtils'
-
-const results = [];
-function getTestData() {
-    const sql = 'select top 10 * from g_grant_loan';
-    SQLSUtils.query(sql, results);
-    console.log(results);
-    return results;
-
-}
-
-
+import Query from  './dao/sqlserverUtils'
 
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          <textarea>
-              { getTestData()}
+
+    constructor(props) {
+        super(props);
+        this.onClick = this.bind.onClick(this);
+        this.getTestData = this.bind.getTestData(this);
+        this.state = {
+            data: 'no data'
+        }
+    }
+
+    onClick() {
+        this.getTestData();
+    }
+
+    getTestData() {
+        var config = {
+            host: '',
+            port: '',
+            username: 'sa',
+            password: 'sa',
+            db: ''
+        };
+        const q = new Query(config);
+        q.table("dbo.g_grant_loan");
+        q.query('select top 10 * from ' + this.tableName, function (res) {
+            this.setState({
+                data: res.recordsets
+            });
+
+        }, function (err) {
+            this.setState({
+                data: 'no data'
+            });
+        });
+    }
+
+    render() {
+
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    <button onClick={this.onClick}/>
+                    <textarea>
+              { this.state.data}
           </textarea>
-        </p>
-      </div>
-    );
-  }
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
